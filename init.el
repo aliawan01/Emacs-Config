06b5746a9cfaf94f5d2730d67f432cb302026bfa
aliawan01 <76787336+aliawan01@users.noreply.github.com>
2021-03-11 12:43:10 +0000

    Update init.el

--- init.el
+++ init.el
@@ -15,11 +15,11 @@
 (global-set-key (kbd "<escape>") 'keyboard-escape-quit)
 (setq inhibit-startup-message t) 
 (tool-bar-mode -1)
-;;(menu-bar-mode -1)
+(menu-bar-mode -1)
 (setq mouse-wheel-scroll-amount '(1 ((shift) . 1)))
 (setq mouse-wheel-progressive-speed nil)
 (setq mouse-wheel-follow-mouse 't)
-;; (setq scroll-step 1)
+(setq scroll-step 1)
 (setq-default tab-width 4)
 (setq-default indent-tabs-mode nil)
 
