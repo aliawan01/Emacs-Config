42c659b5319745a787aaaf8da7e6a07aafc579d8
aliawan01 <76787336+aliawan01@users.noreply.github.com>
2021-09-25 10:27:29 +0100

    Update init.el

--- init.el
+++ init.el
@@ -1,31 +1,34 @@
 (defvar ido-cur-list nil)
 (defun ali/initial-setup ()
-  "Basic Settings to make emacs useable"
-  (interactive)
-  (setq inhibit-startup-message t) 
-  (blink-cursor-mode -1)
-  (tool-bar-mode -1)
-  (menu-bar-mode -1)
-  (scroll-bar-mode -1)
-  (setq mouse-wheel-scroll-amount '(1 ((shift) . 1)))
-  (setq mouse-wheel-progressive-speed nil)
-  (setq mouse-wheel-follow-mouse 't)
-  (setq scroll-step 3)
-  (setq scroll-margin 3)
-  (setq-default indent-tabs-mode nil)
-  (setq debug-on-error t)
-  
-  ;; Setting ESC to quit 
-  (define-key key-translation-map (kbd "ESC") (kbd "C-g"))
-  
-  ;; Making the line numbers wider
-  (add-hook 'display-line-numbers-mode-hook '(lambda() (setq display-line-numbers-width 3)))
-  
+    "Basic Settings to make emacs useable"
+    (interactive)
+    (setq inhibit-startup-message t) 
+    (blink-cursor-mode -1)
+    (tool-bar-mode -1)
+    (menu-bar-mode -1)
+    (scroll-bar-mode -1)
+    (setq mouse-wheel-scroll-amount '(1 ((shift) . 1)))
+    (setq mouse-wheel-progressive-speed nil)
+    (setq mouse-wheel-follow-mouse 't)
+    (setq scroll-step 3)
+    (setq scroll-margin 3)
+    (setq-default indent-tabs-mode nil)
+    (setq debug-on-error t)
+    (setq ring-bell-function 'ignore) 
+    (add-hook 'prog-mode-hook 'highlight-numbers-mode)
+
+    ;; Setting ESC to quit 
+    (define-key key-translation-map (kbd "ESC") (kbd "C-g"))
+
+    ;; Making the line numbers wider
+    (add-hook 'display-line-numbers-mode-hook '(lambda() (setq display-line-numbers-width 3)))
+     
+
   ;; Getting rid of autosave files and other stuff
   (setq make-lockfiles nil
         make-backup-files nil
         auto-save-default nil)
-  
+
   ;; Making splits always be vertical by default
   (setq split-height-threshold nil
           split-width-threshold 0)
@@ -40,14 +43,45 @@
   ;; Getting rid of the annoying curly arrows at the end of folded lines and making the colour of the fringes the same colour as the background
   (setq-default fringe-indicator-alist '(continuation nil nil))
   (set-face-attribute 'fringe nil :background nil)
-  
-  (set-face-attribute 'default nil :font "JetBrains Mono" :height 125)
-  (set-background-color "#ffffd8")
-  (set-foreground-color "black")
+
+  (set-face-attribute 'default nil :font "Liberation Mono" :height 120)
+  ;; (set-background-color "#ffffd8")
+  ;; (set-foreground-color "black")
+  (set-face-attribute 'font-lock-builtin-face nil :foreground "#DAB98F")
+  (set-face-attribute 'font-lock-comment-face nil :foreground "gray50")
+  (set-face-attribute 'font-lock-constant-face nil :foreground "burlywood3")
+  (set-face-attribute 'font-lock-doc-face nil :foreground "gray50")
+  (set-face-attribute 'font-lock-function-name-face nil :foreground "#c76f4a")
+  (set-face-attribute 'font-lock-keyword-face nil :foreground "DarkGoldenrod3")
+  (set-face-attribute 'font-lock-string-face nil :foreground "olive drab")
+  (set-face-attribute 'font-lock-type-face nil :foreground "DarkGoldenrod3")
+  (set-face-attribute 'font-lock-variable-name-face nil :foreground "burlywood3")
+  (set-foreground-color "burlywood3")
+  (set-background-color "#161616")
+  (set-cursor-color "#40FF40")
+
+  (make-face 'font-lock-todo-face)
+  (make-face 'font-lock-note-face)
+
+  (mapc (lambda (mode)
+ 	 (font-lock-add-keywords
+ 	  mode
+ 	  '(("\\<\\(TODO\\)" 1 'font-lock-todo-face t)
+        ("\\<\\(NOTE\\)" 1 'font-lock-note-face t))))
+ 	'(c++-mode c-mode python-mode emacs-lisp-mode))
+
+  (modify-face 'font-lock-todo-face "Red" nil nil t)
+  (modify-face 'font-lock-note-face "chartreuse" nil nil t)
+
 )
 
 ;; Setting up indentation for C/C++ files
-(add-to-list 'auto-mode-alist '("\\.h\\'" . c++-mode))
+(require 'cc-mode)
+(setq auto-mode-alist (append
+            '(("\\.cpp$"    . c++-mode)
+              ("\\.h$"    . c++-mode)
+              ("\\.c$"   . c++-mode)) auto-mode-alist))
+
 (c-add-style "ali-c-indenting" '((c-tab-always-indent . t)
                                  (c-basic-offset . 4)
                                  (c-offsets-alist (access-label . 0)
@@ -67,7 +101,7 @@
 					       arg)))
 		   (arg
 		    (generate-new-buffer eshell-buffer-name))
-		   (t
+\		   (t
 		    (get-buffer-create eshell-buffer-name)))))
     (pop-to-buffer-same-window buf)
     (unless (derived-mode-p 'eshell-mode)
@@ -95,7 +129,7 @@
        "│"
        "\n"
        "╰ "
-       
+    
        (look "aliawan 🔥" :foreground "blue")
        (look " " :foreground "black")
        )))
@@ -137,7 +171,7 @@
   (package-refresh-contents))
 
 ;; Making sure that all the packages are installed
-(setq package-selected-packages '(evil org modus-themes undo-tree))
+(setq package-selected-packages '(evil highlight-numbers undo-tree))
 (package-install-selected-packages)
 
 (defun ali/ido () 
@@ -165,23 +199,37 @@
   (set-transient-map ido-completion-map t)
   (let ((enable-recursive-minibuffers t)
         (minibuffer-depth-indicate-mode 10))
-      (define-key ido-completion-map (kbd "C-l") 'ido-next-match)
-      (define-key ido-completion-map (kbd "M-d") 'ido-enter-dired)
-      (define-key ido-completion-map (kbd "M-f") 'ido-find-file-other-window)
-      (define-key ido-completion-map (kbd "C-h") 'ido-prev-match)))
+        (define-key ido-completion-map (kbd "C-l") 'ido-next-match)
+        (define-key ido-completion-map (kbd "M-d") 'ido-enter-dired)
+        (define-key ido-completion-map (kbd "M-f") 'ido-find-file-other-window)
+        (define-key ido-completion-map (kbd "C-h") 'ido-prev-match)))
 
 (add-hook 'ido-setup-hook 'ido-keybindings)
+
 
 ;; Settings for increasing and decreasing size of the image when in the `image-mode'
 (define-key image-map (kbd "C-=") 'image-increase-size)
 (define-key image-map (kbd "C--") 'image-decrease-size)
-
-(defun ali/org ()
+(defun indent-current-line ()
   (interactive)
-  (require 'org)
-  (org-mode))
+    (progn 
+        (setq regionStart (line-beginning-position))
+        (setq regionEnd (line-end-position))
 
-(require 'modus-themes)
+        (when (use-region-p)
+            (setq regionStart (region-beginning))
+            (setq regionEnd (region-end))
+        )
+        (save-excursion
+            (goto-char regionStart)
+            (setq start (line-beginning-position))
+            (goto-char regionEnd)
+            (setq end (line-end-position))
+            (indent-rigidly start end 4)
+            (setq deactivate-mark nil)
+            ))
+)
+
 
 (defun ali/evil ()
   "Setup for evil mode"
@@ -192,9 +240,7 @@
   ;; Making cursor always be a box
   (setq evil-insert-state-cursor 'box)
 
-  ;; Using undo-tree for undo and redo
-  (define-key evil-normal-state-map (kbd "C-r") 'undo-tree-redo)
-  (define-key evil-normal-state-map (kbd "u") 'undo-tree-undo)
+  (define-key evil-normal-state-map (kbd "C-b c") 'projects)
 
   ;; Balancing all the windows on the screen
   (define-key evil-normal-state-map (kbd "=") 'balance-windows)
@@ -202,26 +248,31 @@
   ;; Swapping the windows on the screen
   (define-key evil-normal-state-map (kbd "+") 'window-swap-states)
 
+  ;; Rebinding : to ; in normal mode
+  (define-key evil-normal-state-map (kbd ";") 'evil-ex)
+
+  ;; Commands for building and running codes
+  (define-key evil-normal-state-map (kbd "C-e") 'build-compile)
+  (define-key evil-normal-state-map (kbd "C-q") 'run-compile)
+
+  ;; Making TAB autocomplete
+  (define-key evil-insert-state-map [tab] 'evil-complete-next)
+
+  ;; Making Shift-Tab indent current line
+  (define-key evil-insert-state-map [backtab] 'indent-current-line)
+
   (evil-make-overriding-map ali-keymap 'normal 'motion)
   (evil-mode 1))
 
-;; Making undo-tree start automatically
-(add-hook 'prog-mode-hook '(lambda()
-                             (interactive)
-                             (require 'undo-tree)
-                             (undo-tree-mode 1)))
 
-(defun undo-tree-hook ()
-  "Hook to make the undo-tree-visualizer window to automatically resize to become smaller"
+(defun ali/undo-tree ()
   (interactive)
+  ;; Settings for undo-tree
+  (with-eval-after-load 'undo-tree (defun undo-tree-overridden-undo-bindings-p () nil))
+  (global-undo-tree-mode 1)
+  (evil-set-undo-system 'undo-tree)
+)
 
-  (unless (not (get-buffer-window " *undo-tree*"))
-    (save-excursion
-            (select-window (get-buffer-window " *undo-tree*"))
-            (evil-window-set-width 45)
-            (windmove-left 1))))
-
-(add-hook 'undo-tree-visualizer-mode-hook 'undo-tree-hook)
 
 (defun ali/compile-python ()
     (interactive)
@@ -241,6 +292,23 @@
   (interactive)
   (backward-up-list))
 
+(defun projects ()
+  (interactive)
+    (switch-to-buffer (find-file-noselect "C:\\Dev\\LearningC++\\main.cpp"))
+)
+
+(defun build-compile ()
+  (interactive)
+  (switch-to-buffer-other-window "*compilation*")
+  (compile "build.bat")
+  (other-window 1))
+
+(defun run-compile ()
+  (interactive)
+  (compile "run.bat"))
+
+;; Making sure that tabs are used in makefiles
+(add-hook 'makefile-mode-hook '(lambda () (interactive) (setq indent-tabs-mode t)))
 
 (let ((ali-keymap (make-sparse-keymap)))
     "Evil keybindings for navigating through windows"
@@ -279,9 +347,6 @@
     "Setting C-s-h for help"
     (define-key ali-keymap (kbd "C-S-h") 'help)
 
-    ;; Toggling the undo tree visualizer
-    (define-key ali-keymap (kbd "C-c u") 'undo-tree-visualize)
-
     "Shortcut to get my init.el"
     (define-key ali-keymap (kbd "M-E") '(lambda() (interactive) (find-file "~/.emacs.d/init.el")))
 
@@ -313,6 +378,7 @@
   :lighter " keys"
   :keymap ali-keymap)
 
+
 (ali-keybindings-mode 1)
 
 (ali/initial-setup)
@@ -320,3 +386,4 @@
 (ali/evil)
 (ali/ido)
 (ali/colours)
+(ali/undo-tree)
